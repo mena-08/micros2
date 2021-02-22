c9fcdab286be3e407d9b0c2ea3cbce6d101f2a0f
mena-08 <reach_71@hotmail.com>
2021-02-21 21:33:45 -0600

    Práctica 2
    Contador decremento o incremento de 0  a 99

--- main.c
+++ main.c
@@ -1,58 +1,50 @@
 /*
- * Titulo PRACTICA1_PEDRO_OMAR
+ * Practica 1 
  *
  * Created: 30/01/2021
- * Author : OMAR MENA; PEDRO GONZALEZ
+ * Author : Omar Mena Facio
  */ 
 
 #define  F_CPU 1000000UL
-#include <avr/io.h>				//Libreria general del Microcontrolador
+#include <avr/io.h>				//Librería general del Microcontrolador
 #include <stdint.h>				//Para poder declarar varialbes especiales
 #include <util/delay.h>			//Para poder hacer delays
 //#include <avr/interrupt.h>	 //Para poder manejar interrupciones
 
 //PROTOTIPADO DE FUNCIONES PARA PODER UTILIZARLAS DESDE CUALQUIER "LUGAR"
-//*************************************************************************
+//*************************
 uint8_t cero_en_bit(volatile uint8_t *LUGAR, uint8_t BIT);
 uint8_t uno_en_bit(volatile uint8_t *LUGAR, uint8_t BIT);
-//*************************************************************************
+volatile uint8_t teclado[4][4] = {{1,2,3,10},
+{4,5,6,11},
+{7,8,9,12},
+{14,0,15,13}};
+uint8_t tecladoFuncion(volatile uint8_t *DDRT, volatile uint8_t *PORTT, volatile uint8_t *PINT);
+
+//*************************
 
 
-int main(void)
-{
-	DDRA = 0;
-	PORTA |= (15<<0);
+int main(void){
 	
-	DDRB = 255;
-	PORTB = 255;
+	DDRA = 0B00000000;
+	PORTA = 0B11111111;
+	
+	DDRC = 0B11111111;
+	PORTC = 0B00000000;
+	uint8_t contador=97;
+		
     /* Codigo principal */
     while (1) 
     {
-		uint8_t digito;
-		digito = PINA;
+		uint8_t lectura = tecladoFuncion(&DDRA,&PORTA,&PINA);
 		
-		//if (cero==0){
-		//	PORTB = 0b00000001;
-		//}
-		if (digito==0b00000001){
-			_delay_ms(100);
-			while(uno_en_bit(&PINA,0)){
-				
-			}
-			_delay_ms(100);
-			PORTB=0b11001111;
+		if (lectura<10)
+		{
+			PORTC=lectura;
 		}
 		
-		
-		
-		/* Las funciones de revisar bits pueden usarse como:
-		   if(cero_en_bit(&PINA,2)) {}
-		   o bien como:
-		   while(cero_en_bit(&PINA,2)){}
-		   entre otras opciones*/
     }
 }
-
 
 
 
@@ -64,4 +56,36 @@ uint8_t cero_en_bit(volatile uint8_t *LUGAR, uint8_t BIT)
 uint8_t uno_en_bit(volatile uint8_t *LUGAR, uint8_t BIT)
 {
 	return (*LUGAR&(1<<BIT));
+}
+
+uint8_t tecladoFuncion(volatile uint8_t *DDRT, volatile uint8_t *PORTT, volatile uint8_t *PINT) {
+	//La funcion tecladoFuncion se quedará trabada hasta el momento en que se presione una tecla
+	//Esta función regresa un valor del tipo uint8_t que tendrá que ser asignada a una variable
+	//Ejemplo:
+
+	//uint8_t tecla;
+	//tecla = tecladoFuncion(&DDRA,&PORTA,&PINA);
+
+	*DDRT = (1<<7)|(1<<6)|(1<<5)|(1<<4);		// definimos puerto para teclado
+	
+	while(1) {
+		
+		*PORTT = 0b11111111;
+		
+		for (uint8_t fila = 7; fila > 3; fila--) {
+			
+			*PORTT = ~(1<<fila);
+			_delay_us(2);
+			
+			for (uint8_t col = 0; col < 4; col++) {
+				
+				if (cero_en_pin(PINT,col)) {
+					_delay_ms(100);
+					while(cero_en_pin(PINT,col)) {}
+					_delay_ms(100);
+					return teclado[7-fila][col];
+				}
+			}
+		}
+	}
 }
